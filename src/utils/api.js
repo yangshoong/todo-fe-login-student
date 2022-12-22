2425b7c2c7f9189a6992c7aa74d69a025d8a5ebf
Bitna Kim <tallshe108@hanmail.net>
2022-12-22 17:44:03 +0700

    login lecture fe for student

--- src/utils/api.js
+++ src/utils/api.js
@@ -4,6 +4,7 @@ const api = axios.create({
   baseURL: `${process.env.REACT_APP_BACKEND_URL}/api`,
   headers: {
     "Content-Type": "application/json",
+    authorization: "Bearer " + localStorage.getItem("token"),
   },
 });
 /**
