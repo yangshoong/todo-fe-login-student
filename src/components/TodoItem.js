e38f1eea459ba71d7254e56f4231d0c8b31b506e
yangshoong <crinaeae83@gmail.com>
2024-05-28 22:35:52 +0900

    렌더링

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,23 +2,24 @@ import React from "react";
 import { Col, Row } from "react-bootstrap";
 
 const TodoItem = ({ item, deleteItem, toggleComplete }) => {
+  const handleDelete = () => {
+    deleteItem(item._id);
+  };
+
+  const handleToggle = () => {
+    toggleComplete(item._id);
+  };
+
   return (
     <Row>
       <Col xs={12}>
         <div className={`todo-item ${item.isComplete ? "item-complete" : ""}`}>
           <div className="todo-content">{item.task}</div>
-
           <div>
-            <button
-              className="button-delete"
-              onClick={() => deleteItem(item._id)}
-            >
+            <button className="button-delete" onClick={handleDelete}>
               삭제
             </button>
-            <button
-              className="button-delete"
-              onClick={() => toggleComplete(item._id)}
-            >
+            <button className="button-delete" onClick={handleToggle}>
               {item.isComplete ? `안끝남` : `끝남`}
             </button>
           </div>
